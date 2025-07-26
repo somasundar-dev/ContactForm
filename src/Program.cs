73a4be308a35037aee5871229e2b3791ecf908bc
Somasundar <somausundar.work@outlook.com>
2025-07-26 21:19:02 +0530

    Update AWS Lambda hosting to use RestApi and enhance CORS policy configuration

--- src/Program.cs
+++ src/Program.cs
@@ -7,25 +7,26 @@ using Contact.API.Services;
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
-builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestApi);
 builder.Services.AddScoped<IEmailService, EmailService>();
 builder.Services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
 builder.Services.Configure<UserInfoOptions>(configuration.GetSection("UserInfo"));
 builder.Services.AddCors(policy =>
 {
-    policy.AddPolicy("Production", builder =>
-    {
-        Console.WriteLine("Configuring CORS policy for production environment...");
-        var allowedOrigins = configuration["AllowedOrigins"];
-        if (string.IsNullOrWhiteSpace(allowedOrigins))
+    policy.AddPolicy(
+        "Production",
+        builder =>
         {
-            ArgumentNullException.ThrowIfNull(allowedOrigins, nameof(allowedOrigins));
-        }
+            Console.WriteLine("Configuring CORS policy for production environment...");
+            var allowedOrigins = configuration["AllowedOrigins"];
+            if (string.IsNullOrWhiteSpace(allowedOrigins))
+            {
+                ArgumentNullException.ThrowIfNull(allowedOrigins, nameof(allowedOrigins));
+            }
 
-        builder.WithOrigins(allowedOrigins.Split(","))
-               .AllowAnyMethod()
-               .AllowAnyHeader();
-    });
+            builder.WithOrigins(allowedOrigins.Split(",")).AllowAnyMethod().AllowAnyHeader();
+        }
+    );
 });
 
 var app = builder.Build();
@@ -40,33 +41,42 @@ app.UseHttpsRedirection();
 
 app.UseCors("Production");
 
-app.MapGet("/", () =>
-{
-    return Results.Ok("Email Service API is Running");
-});
-
-app.MapPost("/send-email", async (EmailRequest emailRequest, CancellationToken cancellationToken, IEmailService emailService) =>
-{
-    cancellationToken.ThrowIfCancellationRequested();
-    var validationResults = new List<ValidationResult>();
-    if (!Validator.TryValidateObject(emailRequest, new ValidationContext(emailRequest), validationResults, true))
+app.MapGet(
+    "/api/contact",
+    () =>
     {
-        return Results.BadRequest(validationResults);
+        return Results.Ok("Email Service API is Running");
     }
+);
 
-    var emailContent = await emailService.GetEmailTemplateAsync(EmailTemplateConstants.Template1, cancellationToken);
-
-    emailContent = emailContent
-        .Replace(EmailTemplateConstants.SubmitterPlaceholder.Name, emailRequest.Name)
-        .Replace(EmailTemplateConstants.SubmitterPlaceholder.Email, emailRequest.Email)
-        .Replace(EmailTemplateConstants.SubmitterPlaceholder.Message, emailRequest.Message);
-
-    var emailSent = await emailService.SendEmailAsync(emailRequest, emailContent, cancellationToken);
-    if (!emailSent)
+app.MapPost(
+    "/api/contact/send-email",
+    async (EmailRequest emailRequest, CancellationToken cancellationToken, IEmailService emailService) =>
     {
-        return Results.BadRequest("Failed to send email.");
+        cancellationToken.ThrowIfCancellationRequested();
+        var validationResults = new List<ValidationResult>();
+        if (!Validator.TryValidateObject(emailRequest, new ValidationContext(emailRequest), validationResults, true))
+        {
+            return Results.BadRequest(validationResults);
+        }
+
+        var emailContent = await emailService.GetEmailTemplateAsync(
+            EmailTemplateConstants.Template1,
+            cancellationToken
+        );
+
+        emailContent = emailContent
+            .Replace(EmailTemplateConstants.SubmitterPlaceholder.Name, emailRequest.Name)
+            .Replace(EmailTemplateConstants.SubmitterPlaceholder.Email, emailRequest.Email)
+            .Replace(EmailTemplateConstants.SubmitterPlaceholder.Message, emailRequest.Message);
+
+        var emailSent = await emailService.SendEmailAsync(emailRequest, emailContent, cancellationToken);
+        if (!emailSent)
+        {
+            return Results.BadRequest("Failed to send email.");
+        }
+        return Results.Ok("Email sent successfully.");
     }
-    return Results.Ok("Email sent successfully.");
-});
+);
 
 app.Run();
